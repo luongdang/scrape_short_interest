827102d337c596087a70b2df6b2af3d2e51db81f
oescca_ldang <ldang@justenergy.com>
2018-11-05 16:36:35 -0500

    Bug fixes

--- scrape.py
+++ scrape.py
@@ -1,8 +1,23 @@
 from shortinterest import ShortInterest
-import locale
+from xlsxwriter import Workbook
 
-locale.setlocale(locale.LC_ALL, "en_US")
+results = ShortInterest.scrape(exchanges=["nyse"], keys=["A"])
+book = Workbook("ShortInterest.xlsx")
+sheet = book.add_worksheet()
 
-results = ShortInterest.scrape()
-for r in results:
-    print(f"{r.symbol}: {r.currentShortInterest}")
+headers = [
+    "Company Name",
+    "Symbol",
+    "Current Short Interest",
+    "Previous Short Interest"
+]
+for col, h in enumerate(headers):
+    sheet.write(0, col, h)
+
+for row, r in enumerate(results):
+    sheet.write(row + 1, 0, r.name )
+    sheet.write(row + 1, 1, r.symbol)
+    sheet.write(row + 1, 2, r.currentShortInterest)
+    sheet.write(row + 1, 3, r.previousShortInterest)
+
+book.close()
--- shortinterest.py
+++ shortinterest.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
-from itertools import groupby
+from itertools import groupby, product
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 import locale
 import platform
 import string
+import sys
 
 class ShortInterest:
     def __init__(self):
@@ -32,11 +33,11 @@ class ShortInterest:
         interest = ShortInterest()
         interest.name                  = columns[0].text
         interest.symbol                = columns[1].text
-        interest.currentShortInterest  = _tryConvert(locale.atoi, columns[2].text)
-        interest.previousShortInterest = _tryConvert(locale.atoi, columns[3].text)
-        interest.percentOfFloat        = _tryConvert(locale.atof, columns[6].text)
-        interest.daysToCover           = _tryConvert(locale.atoi, columns[7].text)
-        interest.averageDailyVolume    = _tryConvert(locale.atoi, columns[8].text)
+        interest.currentShortInterest  = _toFloat(columns[2].text)
+        interest.previousShortInterest = _toFloat(columns[3].text)
+        interest.percentOfFloat        = _toFloat(columns[6].text)
+        interest.daysToCover           = _toFloat(columns[7].text)
+        interest.averageDailyVolume    = _toFloat(columns[8].text)
         #
         interest.currentReportDate     = currentReportDate
         interest.previousReportDate    = previousReportDate
@@ -44,79 +45,78 @@ class ShortInterest:
         return interest
 
     @classmethod
-    def scrape(cls):
+    def scrape(cls, exchanges=["nyse", "nasdaq", "amex"], keys=None):
         # A `driver` allows us to drive Chrome from Python. To the WSJ, this is
         # indistinguiable from a user manually visiting the page.
         driver = _getChromeDriver()
 
-        # The three exchanges available on the site
-        exchanges = ["nyse", "nasdaq", "amex"]
-
         # A list containing the letters "a" to "z", plus the string "0-9"
-        keys = list(string.ascii_uppercase) + ['0-9']
+        if keys is None:
+            keys = list(string.ascii_uppercase) + ['0_9']
 
         # The result of the scrape
         results = []
+        for exchange, key in product(exchanges, keys):
+            url = f"http://www.wsj.com/mdc/public/page/2_3062-sht{exchange}_{key}-listing.html"
+            print(f"Scraping from {url}")
 
-        for exchange in exchanges:
-            for key in keys:
-                url = f"http://www.wsj.com/mdc/public/page/2_3062-sht{exchange}_{key}-listing.html"
-                print(f"Scraping from {url}")
+            # Open the page in Chrome. We are driving Chrome from Python
+            driver.get(url)
 
-                # Open the page in Chrome. We are driving Chrome from Python
-                driver.get(url)
+            try:
+                # Never parse HTML using RegEx. Always use a proper parser like
+                # BeautifulSoup.
+                html = driver.find_element(By.CSS_SELECTOR, "table.mdcTable").get_attribute("innerHTML")
+                table = BeautifulSoup(html, "html.parser")
 
-                try:
-                     # Never parse HTML using RegEx. Always use a proper parser
-                     # like BeautifulSoup.
-                    soup = BeautifulSoup(driver.find_element(By.TAG_NAME, "html").innerHTML)
+                # table.mdcTable is the CSS selector for "the <table> element
+                # with class mdcTable"
+                # table = soup.select_one("table.mdcTable")
 
-                    # table.mdcTable is the CSS selector for "the <table> element
-                    # with class mdcTable"
-                    table = soup.select_one("table.mdcTable")
+                # This is Python tuple-spread syntax:
+                #     x, y = 1, 2
+                # means the same thing as:
+                #     x = 1
+                #     y = 2
+                currentReportDate, previousReportDate = None, None
+                count = 0
 
-                    # This is Python tuple-spread syntax:
-                    #     x, y = 1, 2
-                    # means the same thing as:
-                    #     x = 1
-                    #     y = 2
-                    currentReportDate, previousReportDate = None, None
-                    count = 0
+                for index, row in enumerate(table.select("tr")):
+                    columns = row.select("td")
 
-                    for index, row in enumerate(table.select("tr")):
-                        columns = row.select("td")
+                    if index == 0:
+                        formatString = "%m/%d/%y"
+                        currentReportDate  = datetime.strptime(columns[2].text, formatString)
+                        previousReportDate = datetime.strptime(columns[3].text, formatString)
+                    else:
+                        count += 1
+                        results.append(ShortInterest.__createFromColumns(columns, currentReportDate, previousReportDate))
 
-                        if index == 0:
-                            formatString = "%m/%d/%y"
-                            currentReportDate  = datetime.strptime(columns[2].text, formatString)
-                            previousReportDate = datetime.strptime(columns[3].text, formatString)
-                        else:
-                            count += 1
-                            results.append(ShortInterest.__createFromColumns(columns, currentReportDate, previousReportDate))
-
-                    print(f"Scraped {count} symbols")
-                except:
-                    print(f"Skipping '{exchange}' and '{key}'")
+                print(f"Scraped {count} symbols")
+            except:
+                print(f"Error parsing '{exchange}' and '{key}': {sys.exc_info()[1]}")
 
         driver.quit()
         return results
 
-def _tryConvert(convertFunc, str, defaultValue=None):
+
+def _toFloat(str, defaultValue=None):
     try:
-        return convertFunc(str)
-    except ValueError:
+        return float(str.replace(",", ""))
+    except:
         return defaultValue
+
 
 def _getChromeDriver():
     osName = platform.system()
     if osName == "Darwin":
-        driverPath = "drivers/chromedriver_mac64"
+        driverPath = "./drivers/chromedriver_mac64"
     elif osName == "Windows":
-        driverPath = "drivers/chromedriver_win32"
+        driverPath = "./drivers/chromedriver_win32"
     elif osName == "Linux":
-        driverPath = "drivers/chromedriver_linux64"
+        driverPath = "./drivers/chromedriver_linux64"
     else:
-        raise Exception(f"No Chrome driver for platform '{osName}'")
+        raise Exception(f"No Chrome driver for operating system '{osName}'")
 
     options = webdriver.ChromeOptions()
 
